0efad324e497994febe9847de066c90c2c94c33b
SirCancleALot <f.fraeulin@gmx.de>
2018-09-26 15:18:36 +0200

    filled in the bytes to communicate with the device and first idea of the getDistance() method

--- src/main/java/com/tsystems/mms/evaluate/srf02/Srf02Configuration.java
+++ src/main/java/com/tsystems/mms/evaluate/srf02/Srf02Configuration.java
@@ -1,8 +1,19 @@
 package com.tsystems.mms.evaluate.srf02;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "srf02")
 public class Srf02Configuration {
-	// TODO
+    private Srf02Connector connector;
+
+    private Srf02Configuration(@Autowired Srf02Connector connector){
+        this.connector = connector;
+    }
+
+    private findPort
+
+    public Srf02Connector getConnector() {
+        return connector;
+    }
 }
